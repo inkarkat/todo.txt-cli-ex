bec4b2501d401b6020c45869bce4b64a0cdf4b0e
Ingo Karkat <swdev@ingo-karkat.de>
2012-04-11 14:39:47 +0200

    Also test effective tasks resulting from wait.

--- tests/t2000-wait.sh
+++ tests/t2000-wait.sh
@@ -27,6 +27,15 @@ TODO: 2 is blocked due to w:money.
 >>> todo.sh -p wait 2 "salary increase"
 2 2012-02-01 obtain a bank loan w:money w:(salary increase)
 TODO: 2 is blocked due to w:(salary increase).
+
+>>> todo.sh -p command ls
+5 2011-01-01 build your dream home
+1 2011-01-01 find a building site
+3 2012-02-01 buy the site
+2 2012-02-01 obtain a bank loan w:money w:(salary increase)
+4 2012-02-02 hire an architect
+--
+TODO: 5 of 5 tasks shown
 EOF
 
 set_up
@@ -43,6 +52,15 @@ test_todo_session 'wait for DEPITEM#' <<EOF
 >>> todo.sh -p wait 3 for 2
 3 2012-02-01 buy the site w:2
 TODO: 3 has become dependent on 2.
+
+>>> todo.sh -p command ls
+5 2011-01-01 build your dream home
+1 2011-01-01 find a building site
+3 2012-02-01 buy the site w:2
+2 2012-02-01 obtain a bank loan
+4 2012-02-02 hire an architect
+--
+TODO: 5 of 5 tasks shown
 EOF
 
 set_up
