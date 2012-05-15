01b5c7573e66e5ff3bcae42ec0cb144c19a422e6
Ingo Karkat <swdev@ingo-karkat.de>
2012-05-15 12:35:31 +0200

    lsbumped: Suppress done and trashed tasks.

--- tests/t2120-lsbumped.sh
+++ tests/t2120-lsbumped.sh
@@ -43,4 +43,20 @@ test_todo_session 'lsbumped TERM' <<EOF
 TODO: 2 bumped of 5 tasks shown
 EOF
 
+cat > todo.txt <<EOF
+2011-08-08 mow the lawn !:3 and cut the trees @outside
+x 2012-01-01 2011-09-09 walk the dog !:1 @outside
+2011-09-09 watch tv !:1
+X 2012-01-01 2011-10-10 clean the bike !:11
+2011-11-11 buy beer
+EOF
+
+test_todo_session 'lsbumped done and trashed' <<EOF
+>>> todo.sh -p lsbumped
+1 2011-08-08 mow the lawn !:3 and cut the trees @outside
+3 2011-09-09 watch tv !:1
+--
+TODO: 2 bumped of 5 tasks shown
+EOF
+
 test_done
