89fcba0595da4184748133eef1a4ec766a29caa5
Ingo Karkat <swdev@ingo-karkat.de>
2012-05-15 12:29:45 +0200

    Add lsbumped action.

---
+++ tests/t2120-lsbumped.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#
+
+test_description='lsbumped action
+
+This test covers listing bumped tasks.
+'
+. ./test-lib.sh
+
+cat > todo.txt <<EOF
+2011-11-11 buy beer
+EOF
+test_todo_session 'lsbumped no bumps' <<EOF
+>>> todo.sh -p lsbumped
+--
+TODO: 0 bumped of 1 tasks shown
+EOF
+
+
+cat > todo.txt <<EOF
+2011-08-08 mow the lawn !:3 and cut the trees @outside
+2011-09-09 walk the dog !:1 @outside
+2011-09-09 watch tv !:1
+2011-10-10 clean the bike !:11
+2011-11-11 buy beer
+EOF
+
+test_todo_session 'lsbumped' <<EOF
+>>> todo.sh -p lsbumped
+4 2011-10-10 clean the bike !:11
+1 2011-08-08 mow the lawn !:3 and cut the trees @outside
+3 2011-09-09 watch tv !:1
+2 2011-09-09 walk the dog !:1 @outside
+--
+TODO: 4 bumped of 5 tasks shown
+EOF
+
+test_todo_session 'lsbumped TERM' <<EOF
+>>> todo.sh -p lsbumped @outside
+1 2011-08-08 mow the lawn !:3 and cut the trees @outside
+2 2011-09-09 walk the dog !:1 @outside
+--
+TODO: 2 bumped of 5 tasks shown
+EOF
+
+test_done
