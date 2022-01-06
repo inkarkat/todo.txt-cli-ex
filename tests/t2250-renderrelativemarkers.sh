9000a23935de2fe4cd2cbf11a69c255d1ab69628
Ingo Karkat <swdev@ingo-karkat.de>
2022-01-06 11:19:04 +0100

    Tests: renderrelativemarkers: Add coverage for non-markers

--- tests/t2250-renderrelativemarkers.sh
+++ tests/t2250-renderrelativemarkers.sh
@@ -50,4 +50,24 @@ test_todo_session 'rendering relative task references in markers' <<EOF
 TODO: 5 of 5 tasks shown
 EOF
 
+
+cat > todo.txt <<EOF
+2009-02-01 a simple task
+2009-02-01 date format non-marker seen:14-Feb-2009
+2009-02-01 non-marker https://www.example.com/
+2009-02-01 non-marker task reference file:.-1
+EOF
+
+test_todo_session 'non-markers are not rendered' <<EOF
+>>> TODOTXT_VERBOSE=0 todo.sh renderrelativemarkers
+
+>>> todo.sh -p -x list
+1 2009-02-01 a simple task
+2 2009-02-01 date format non-marker seen:14-Feb-2009
+3 2009-02-01 non-marker https://www.example.com/
+4 2009-02-01 non-marker task reference file:.-1
+--
+TODO: 4 of 4 tasks shown
+EOF
+
 test_done
