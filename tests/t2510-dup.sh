bd72683d7e8440e5446a916e8b485a6117830e75
Ingo Karkat <swdev@ingo-karkat.de>
2026-07-11 12:14:37 +0200

    Tests: Add coverage for deleting / trashing that are referenced by other tasks
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: bash 5.0.17(1)-release

--- tests/t2510-dup.sh
+++ tests/t2510-dup.sh
@@ -65,6 +65,38 @@ TODO: 9 deleted.
 EOF
 
 setup
+test_todo_session 'delete duplicate task that is referenced' <<EOF
+>>> todo.sh -p command ls
+1 2011-01-01 find a building site a:5 +house
+2 2012-02-02 obtain a bank loan +house w:money k:5
+3 2012-02-03 hire an architect +house w:1 w:2
+4 2012-03-01 build your dream home +house w:3 k:6
+5 2012-03-18 buy new walking shoes
+6 2012-03-18 go jogging
+7 2012-03-19 clean up repair shop
+8 2012-03-20 enroll in swimming lessons
+9 2012-03-21 fix old running shoes
+--
+TODO: 9 of 9 tasks shown
+
+>>> todo.sh -a -f dup 9 del 6
+6 2012-03-18 go jogging => dup of 9
+TODO: 6 deleted.
+
+>>> todo.sh -p command ls
+1 2011-01-01 find a building site a:5 +house
+2 2012-02-02 obtain a bank loan +house w:money k:5
+3 2012-02-03 hire an architect +house w:1 w:2
+4 2012-03-01 build your dream home +house w:3 k:6
+5 2012-03-18 buy new walking shoes
+7 2012-03-19 clean up repair shop
+8 2012-03-20 enroll in swimming lessons
+9 2012-03-21 fix old running shoes
+--
+TODO: 8 of 8 tasks shown
+EOF
+
+setup
 test_todo_session 'trash duplicate task' <<EOF
 >>> todo.sh -p command ls
 1 2011-01-01 find a building site a:5 +house
@@ -82,6 +114,39 @@ TODO: 9 of 9 tasks shown
 >>> todo.sh -a -f dup 6 trash 9
 9 X 2009-02-13 2012-03-21 fix old running shoes => dup of 6
 TODO: 9 trashed.
+EOF
+
+setup
+test_todo_session 'trash duplicate task that is referenced' <<EOF
+>>> todo.sh -p command ls
+1 2011-01-01 find a building site a:5 +house
+2 2012-02-02 obtain a bank loan +house w:money k:5
+3 2012-02-03 hire an architect +house w:1 w:2
+4 2012-03-01 build your dream home +house w:3 k:6
+5 2012-03-18 buy new walking shoes
+6 2012-03-18 go jogging
+7 2012-03-19 clean up repair shop
+8 2012-03-20 enroll in swimming lessons
+9 2012-03-21 fix old running shoes
+--
+TODO: 9 of 9 tasks shown
+
+>>> todo.sh -a -f dup 9 trash 6
+6 X 2009-02-13 2012-03-18 go jogging => dup of 9
+TODO: 6 trashed.
+
+>>> todo.sh -p -x command ls
+1 2011-01-01 find a building site a:5 +house
+2 2012-02-02 obtain a bank loan +house w:money k:5
+3 2012-02-03 hire an architect +house w:1 w:2
+4 2012-03-01 build your dream home +house w:3 k:6
+5 2012-03-18 buy new walking shoes
+7 2012-03-19 clean up repair shop
+8 2012-03-20 enroll in swimming lessons
+9 2012-03-21 fix old running shoes
+6 X 2009-02-13 2012-03-18 go jogging => dup of 9
+--
+TODO: 9 of 9 tasks shown
 EOF
 
 setup
