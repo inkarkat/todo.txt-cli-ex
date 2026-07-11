5f81510238637af410a537beffa839ecbe14f01b
Ingo Karkat <swdev@ingo-karkat.de>
2026-07-11 12:23:24 +0200

    Tests: Config: Actually, task 5 is the dup of 9
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: bash 5.0.17(1)-release

--- tests/t2510-dup.sh
+++ tests/t2510-dup.sh
@@ -37,8 +37,8 @@ test_todo_session 'delete duplicate task' <<EOF
 --
 TODO: 9 of 9 tasks shown
 
->>> todo.sh -a -f dup 6 del 9
-9 2012-03-21 fix old running shoes => dup of 6
+>>> todo.sh -a -f dup 5 del 9
+9 2012-03-21 fix old running shoes => dup of 5
 TODO: 9 deleted.
 EOF
 
@@ -57,10 +57,10 @@ test_todo_session 'delete two duplicate tasks' <<EOF
 --
 TODO: 9 of 9 tasks shown
 
->>> todo.sh -a -f dup 6 del 7 9
-7 2012-03-19 clean up repair shop => dup of 6
+>>> todo.sh -a -f dup 5 del 7 9
+7 2012-03-19 clean up repair shop => dup of 5
 TODO: 7 deleted.
-9 2012-03-21 fix old running shoes => dup of 6
+9 2012-03-21 fix old running shoes => dup of 5
 TODO: 9 deleted.
 EOF
 
@@ -79,16 +79,16 @@ test_todo_session 'delete duplicate task that is referenced' <<EOF
 --
 TODO: 9 of 9 tasks shown
 
->>> todo.sh -a -f dup 9 del 6
-6 2012-03-18 go jogging => dup of 9
-TODO: 6 deleted.
+>>> todo.sh -a -f dup 9 del 5
+5 2012-03-18 buy new walking shoes => dup of 9
+TODO: 5 deleted.
 
 >>> todo.sh -p command ls
 1 2011-01-01 find a building site a:5 +house
 2 2012-02-02 obtain a bank loan +house w:money k:5
 3 2012-02-03 hire an architect +house w:1 w:2
 4 2012-03-01 build your dream home +house w:3 k:6
-5 2012-03-18 buy new walking shoes
+6 2012-03-18 go jogging
 7 2012-03-19 clean up repair shop
 8 2012-03-20 enroll in swimming lessons
 9 2012-03-21 fix old running shoes
@@ -111,8 +111,8 @@ test_todo_session 'trash duplicate task' <<EOF
 --
 TODO: 9 of 9 tasks shown
 
->>> todo.sh -a -f dup 6 trash 9
-9 X 2009-02-13 2012-03-21 fix old running shoes => dup of 6
+>>> todo.sh -a -f dup 5 trash 9
+9 X 2009-02-13 2012-03-21 fix old running shoes => dup of 5
 TODO: 9 trashed.
 EOF
 
@@ -131,20 +131,20 @@ test_todo_session 'trash duplicate task that is referenced' <<EOF
 --
 TODO: 9 of 9 tasks shown
 
->>> todo.sh -a -f dup 9 trash 6
-6 X 2009-02-13 2012-03-18 go jogging => dup of 9
-TODO: 6 trashed.
+>>> todo.sh -a -f dup 9 trash 5
+5 X 2009-02-13 2012-03-18 buy new walking shoes => dup of 9
+TODO: 5 trashed.
 
 >>> todo.sh -p -x command ls
 1 2011-01-01 find a building site a:5 +house
 2 2012-02-02 obtain a bank loan +house w:money k:5
 3 2012-02-03 hire an architect +house w:1 w:2
 4 2012-03-01 build your dream home +house w:3 k:6
-5 2012-03-18 buy new walking shoes
+6 2012-03-18 go jogging
 7 2012-03-19 clean up repair shop
 8 2012-03-20 enroll in swimming lessons
 9 2012-03-21 fix old running shoes
-6 X 2009-02-13 2012-03-18 go jogging => dup of 9
+5 X 2009-02-13 2012-03-18 buy new walking shoes => dup of 9
 --
 TODO: 9 of 9 tasks shown
 EOF
@@ -164,8 +164,8 @@ test_todo_session 'immediate (hard) trash duplicate task' <<EOF
 --
 TODO: 9 of 9 tasks shown
 
->>> todo.sh -a -f dup 6 trash --hard 9
-1 X 2009-02-13 2012-03-21 fix old running shoes => dup of 6
+>>> todo.sh -a -f dup 5 trash --hard 9
+1 X 2009-02-13 2012-03-21 fix old running shoes => dup of 5
 TODO: 9 put into TRASH as 1.
 EOF
 
