b4be31cf7c8bfc6b20984f8cb5c91241bb5ae4e4
Ingo Karkat <swdev@ingo-karkat.de>
2026-07-11 15:49:05 +0200

    addm: ENH: Allow reading tasks from stdin via -
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: bash 5.0.17(1)-release

--- tests/t2320-addm.sh
+++ tests/t2320-addm.sh
@@ -8,6 +8,36 @@ This test covers adding tasks (one per line).
 . ./test-lib.sh
 
 : > todo.txt
+test_todo_session 'addm - adds single line standard input' <<EOF
+>>> echo pick some flowers | todo.sh addm -
+1 pick some flowers
+TODO: 1 added.
+EOF
+
+: > todo.txt
+test_todo_session 'addm - adds multi-line standard input as individual tasks' <<EOF
+>>> printf $'pick some flowers\ncut some wood\nrelax in the forest\n' | todo.sh addm -
+1 pick some flowers
+TODO: 1 added.
+2 cut some wood
+TODO: 2 added.
+3 relax in the forest
+TODO: 3 added.
+EOF
+
+: > todo.txt
+test_todo_session 'addm - adds multi-line standard input as individual tasks with custom prefixes and suffixes' <<EOF
+>>> printf $'pick some flowers\ncut some wood\nrelax in the forest\n' | todo.sh addm -b +gardening -e @outside -
+1 +gardening pick some flowers @outside
+TODO: 1 added.
+2 +gardening cut some wood @outside
+TODO: 2 added.
+3 +gardening relax in the forest @outside
+TODO: 3 added.
+EOF
+
+
+: > todo.txt
 test_todo_session 'addm adds individual passed lines as individual tasks' <<EOF
 >>> todo.sh addm $'pick some flowers\ncut some wood\nrelax in the forest'
 1 pick some flowers
