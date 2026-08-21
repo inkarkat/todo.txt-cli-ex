4abf60828abff2a4b6eb1f85ffdf8c7804cfdf82
Ingo Karkat <swdev@ingo-karkat.de>
2026-08-21 16:29:37 +0200

    uncomment: ENH: Allow passing REPLACEMENT comment
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: bash 5.0.17(1)-release

--- tests/t2700-uncomment.sh
+++ tests/t2700-uncomment.sh
@@ -11,7 +11,6 @@ cat > todo.txt <<EOF
 X 2011-08-10 2011-08-08 mow the lawn => too tired
 x 2011-08-10 2011-08-08 buy groceries
 EOF
-
 test_todo_session 'uncomment removes the comment or complains' <<EOF
 >>> todo.sh -p -x lstrashable
 1 X 2011-08-10 2011-08-08 mow the lawn => too tired
@@ -35,12 +34,38 @@ TODO: Task 2 does not contain a comment.
 EOF
 
 cat > todo.txt <<EOF
+X 2011-08-10 2011-08-08 nobody commented text => 
+x 2011-08-10 2011-08-08 buy groceries
+EOF
+test_todo_session 'uncomment -q does not complain if no comment' <<'EOF'
+>>> todo.sh -p -x lsdo
+2 x 2011-08-10 2011-08-08 buy groceries
+--
+TODO: 1 of 2 tasks shown
+
+>>> todo.sh -x uncomment -q 2
+
+>>> todo.sh -x uncomment -q 2 irrelevant new comment
+EOF
+test_todo_session 'uncomment -q confirms removal of empty comment' <<'EOF'
+>>> todo.sh -p -x lstrashable
+1 X 2011-08-10 2011-08-08 nobody commented text => 
+--
+TODO: 1 of 2 tasks shown
+
+>>> printf y | todo.sh -x uncomment -q 1
+\
+1 X 2011-08-10 2011-08-08 nobody commented text => 
+TODO: Replaced task with:
+1 X 2011-08-10 2011-08-08 nobody commented text
+EOF
+
+cat > todo.txt <<EOF
 x 2011-08-10 2011-08-08 mow the lawn => too tired
 x 2011-08-10 2011-08-08 pick some flowers => sunny day
 x 2011-08-10 2011-08-08 wash the car => very dirty
 EOF
-
-test_todo_session 'uncomment -q does not complain if no comment and queries what to do' <<'EOF'
+test_todo_session 'uncomment -q queries what to do' <<'EOF'
 >>> todo.sh -p -x lsdo
 1 x 2011-08-10 2011-08-08 mow the lawn => too tired
 2 x 2011-08-10 2011-08-08 pick some flowers => sunny day
@@ -66,9 +91,50 @@ TODO: Replaced task with:
 EOF
 
 cat > todo.txt <<EOF
- => weird, comment-only task
+X 2011-08-10 2011-08-08 mow the lawn => too tired
+EOF
+test_todo_session 'uncomment replaces the comment' <<EOF
+>>> todo.sh -p -x lstrashable
+1 X 2011-08-10 2011-08-08 mow the lawn => too tired
+--
+TODO: 1 of 1 tasks shown
+
+>>> todo.sh -x uncomment 1 after the mower gets fixed
+1 X 2011-08-10 2011-08-08 mow the lawn => too tired
+TODO: Replaced task with:
+1 X 2011-08-10 2011-08-08 mow the lawn => after the mower gets fixed
+
+>>> todo.sh -p -x lstrashable
+1 X 2011-08-10 2011-08-08 mow the lawn => after the mower gets fixed
+--
+TODO: 1 of 1 tasks shown
 EOF
 
+cat > todo.txt <<EOF
+x 2011-08-10 2011-08-08 mow the lawn => too tired
+x 2011-08-10 2011-08-08 pick some flowers => sunny day
+EOF
+test_todo_session 'uncomment -q confirms replacement' <<'EOF'
+>>> todo.sh -p -x lsdo
+1 x 2011-08-10 2011-08-08 mow the lawn => too tired
+2 x 2011-08-10 2011-08-08 pick some flowers => sunny day
+--
+TODO: 2 of 2 tasks shown
+
+>>> printf y | todo.sh -x uncomment -q 1 after the mower gets fixed
+\
+1 x 2011-08-10 2011-08-08 mow the lawn => too tired
+TODO: Replaced task with:
+1 x 2011-08-10 2011-08-08 mow the lawn => after the mower gets fixed
+
+>>> printf n | todo.sh -x uncomment -q 2 after the mower gets fixed
+\
+=== 4
+EOF
+
+cat > todo.txt <<EOF
+ => weird, comment-only task
+EOF
 test_todo_session 'uncommenting of comment-only task fails' <<EOF
 >>> todo.sh -x uncomment 1
 TODO: Removal of comment would create empty task.
