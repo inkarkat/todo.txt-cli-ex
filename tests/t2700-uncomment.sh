6ae293964c8e65198a393cbfa2f799ff8c3538e4
Ingo Karkat <swdev@ingo-karkat.de>
2026-08-21 15:27:35 +0200

    Tests: Add coverage for uncomment
    System-info: Ubuntu 20.04.6 LTS, x86_64
    Platform-info: bash 5.0.17(1)-release

---
+++ tests/t2700-uncomment.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+#
+
+test_description='uncomment action
+
+This test covers uncommenting.
+'
+. ./test-lib.sh
+
+cat > todo.txt <<EOF
+X 2011-08-10 2011-08-08 mow the lawn => too tired
+x 2011-08-10 2011-08-08 buy groceries
+EOF
+
+test_todo_session 'uncomment removes the comment or complains' <<EOF
+>>> todo.sh -p -x lstrashable
+1 X 2011-08-10 2011-08-08 mow the lawn => too tired
+--
+TODO: 1 of 2 tasks shown
+
+>>> todo.sh -x uncomment 1
+1 X 2011-08-10 2011-08-08 mow the lawn => too tired
+TODO: Replaced task with:
+1 X 2011-08-10 2011-08-08 mow the lawn
+
+>>> todo.sh -p -x lsdo
+2 x 2011-08-10 2011-08-08 buy groceries
+--
+TODO: 1 of 2 tasks shown
+
+>>> todo.sh -x uncomment 2
+2 x 2011-08-10 2011-08-08 buy groceries
+TODO: Task 2 does not contain a comment.
+=== 1
+EOF
+
+cat > todo.txt <<EOF
+x 2011-08-10 2011-08-08 mow the lawn => too tired
+x 2011-08-10 2011-08-08 pick some flowers => sunny day
+x 2011-08-10 2011-08-08 wash the car => very dirty
+EOF
+
+test_todo_session 'uncomment -q does not complain if no comment and queries what to do' <<'EOF'
+>>> todo.sh -p -x lsdo
+1 x 2011-08-10 2011-08-08 mow the lawn => too tired
+2 x 2011-08-10 2011-08-08 pick some flowers => sunny day
+3 x 2011-08-10 2011-08-08 wash the car => very dirty
+--
+TODO: 3 of 3 tasks shown
+
+>>> printf y | todo.sh -x uncomment -q 1
+\
+1 x 2011-08-10 2011-08-08 mow the lawn => too tired
+TODO: Replaced task with:
+1 x 2011-08-10 2011-08-08 mow the lawn
+
+>>> printf n | todo.sh -x uncomment -q 2
+\
+=== 4
+
+>>> printf d | todo.sh -x uncomment -q 3
+\
+3 x 2011-08-10 2011-08-08 wash the car => very dirty
+TODO: Replaced task with:
+3 x 2011-08-10 2011-08-08 wash the car; very dirty
+EOF
+
+cat > todo.txt <<EOF
+ => weird, comment-only task
+EOF
+
+test_todo_session 'uncommenting of comment-only task fails' <<EOF
+>>> todo.sh -x uncomment 1
+TODO: Removal of comment would create empty task.
+=== 1
+EOF
+
+test_done
